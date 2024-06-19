2b80dc9dbfa858c585065c3a13aa61995f309a9c
William Simpson <ws@Williams-Mac-Studio.local>
2024-06-19 15:02:26 -0400

    chore: added text to image script for sd3

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # sd3Diffusers
+
+
+Welcome to sd3Diffusers!
+
+This project serves as a playground for testing out Stable Diffusion 3 (SD3) using Python scripts. The main objective of this project is to explore the capabilities of the SD3 model and discover the exciting possibilities it offers.
+
+If you encounter any issues or have questions, please don't hesitate to open an issue on the repository's GitHub page. We encourage collaboration and are always happy to help.
+
+Let's dive into the world of Stable Diffusion 3 and see what amazing things we can achieve together!
+
+Happy coding!
---
+++ text2image.py
@@ -0,0 +1,32 @@
+import torch
+from diffusers import StableDiffusion3Pipeline
+from transformers import T5EncoderModel
+from PIL import Image
+import sys
+
+def main(prompt, filename="sd3_hello_world.png", steps=25):
+    pipe = StableDiffusion3Pipeline.from_pretrained("stabilityai/stable-diffusion-3-medium-diffusers",torch_dtype=torch.float16)
+    pipe.to("mps")
+
+    image_data = pipe(
+        prompt,
+        negative_prompt="",
+        num_inference_steps=steps,
+        height=1024,
+        width=1024,
+        guidance_scale=7.0,
+    ).images[0]
+    
+    image_data.save(filename)
+    image_data.show()
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script.py [prompt] [filename] [steps]")
+        sys.exit(1)
+        
+    prompt = sys.argv[1]
+    filename = sys.argv[2] if len(sys.argv) > 2 else "sd3_hello_world.png"
+    steps = int(sys.argv[3]) if len(sys.argv) > 3 else 25
+    
+    main(prompt, filename, steps)
