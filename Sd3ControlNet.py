7f7edf7acbf9c61919fd58f0164a3c2d4a3dcd54
WSINTRA <will.simpson85@gmail.com>
2024-06-21 21:28:49 -0400

    feat: added a controlNet for sd3

--- Sd3ControlNet.py
+++ Sd3ControlNet.py
@@ -1,24 +1,36 @@
 import torch
+import sys
 from diffusers import StableDiffusion3ControlNetPipeline
 from diffusers.models import SD3ControlNetModel
 from diffusers.utils import load_image
 
-# load pipeline
-controlnet = SD3ControlNetModel.from_pretrained("InstantX/SD3-Controlnet-Canny")
-pipe = StableDiffusion3ControlNetPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-3-medium-diffusers",
-    controlnet=controlnet
-)
-pipe.to("mps", torch.float16)
+def main(prompt, filename, controlImage):
+    # load pipeline
+    controlnet = SD3ControlNetModel.from_pretrained("InstantX/SD3-Controlnet-Canny")
+    pipe = StableDiffusion3ControlNetPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-3-medium-diffusers",
+        controlnet=controlnet
+    )
+    pipe.to("mps", torch.float16)
 
-# config
-control_image = load_image("logoCanny.png")
-prompt = 'logo etched bevel carved into a wall along with graffiti in an urban dystopia, the wall is crumbling and posted with old soviet posters and there is a murder of crows on the floor, the style of the image is film noire'
-n_prompt = 'NSFW, nude, naked, porn, ugly'
-image = pipe(
-    prompt, 
-    negative_prompt=n_prompt, 
-    control_image=control_image, 
-    controlnet_conditioning_scale=0.75,
-).images[0]
-image.save('image.jpg')
+    control_image = load_image(controlImage)
+    prompt = prompt
+    n_prompt = 'NSFW, nude, naked, porn, ugly'
+    image = pipe(
+        prompt, 
+        negative_prompt=n_prompt, 
+        control_image=control_image, 
+        controlnet_conditioning_scale=0.75,
+    ).images[0]
+    image.save(filename)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: python script.py [prompt] [filename path] [controlImage path]")
+        sys.exit(1)
+        
+    prompt = sys.argv[1]
+    filename = sys.argv[2] + ".png" 
+    controlImage = sys.argv[3] 
+    
+    main(prompt, filename, controlImage)
--- text2image.py
+++ text2image.py
@@ -2,14 +2,13 @@ import torch
 from diffusers import StableDiffusion3Pipeline
 import sys
 
-def main(prompt, filename="sd3_hello_world.png", steps=25):
+def main(prompt, filename, steps=25):
     pipe = StableDiffusion3Pipeline.from_pretrained(
         "stabilityai/stable-diffusion-3-medium-diffusers",
         text_encoder_3=None,
         tokenizer_3=None,
         torch_dtype=torch.float16
         ).to("mps")
-
     image_data = pipe(
         prompt,
         negative_prompt="",
@@ -18,9 +17,7 @@ def main(prompt, filename="sd3_hello_world.png", steps=25):
         width=1024,
         guidance_scale=7.0,
     ).images[0]
-    
     image_data.save(filename)
-    image_data.show()
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -28,7 +25,7 @@ if __name__ == "__main__":
         sys.exit(1)
         
     prompt = sys.argv[1]
-    filename = sys.argv[2] if len(sys.argv) > 2 else "sd3_hello_world.png"
-    steps = int(sys.argv[3]) if len(sys.argv) > 3 else 25
+    filename = sys.argv[2] + ".png"
+    steps = int(sys.argv[3])
     
     main(prompt, filename, steps)
